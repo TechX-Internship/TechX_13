6794f49a1adb3b66b8239e675219fa99d49c9b87
vikas <vikasborse000@gmail.com>
2023-12-29 20:21:51 +0530

    header add

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import Header from '@/component/Header'
+import Image from 'next/image'
+import About from '../component/About'
+
+export default function Home() {
+
+  console.log('first')
+  return (
+    <>
+    
+      <h1>Home</h1>
+      
+     
+    </>
+  )
+}
---
+++ src/component/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-rose-500'>Footer</div>
+  )
+}
+
+export default Footer
